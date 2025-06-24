40686478b5e131b50491763cf9948fdcd3015b1b
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-24 16:31:12 -0400

    Update `RSpec::Runner`

--- lib/glass_sphere/rspec/runner.rb
+++ lib/glass_sphere/rspec/runner.rb
@@ -2,9 +2,32 @@
 
 module GlassSphere
   module RSpec
-    module Runner
-      def self.invoke
-        todo!('GlassSphere::RSpec::Runner.invoke')
+    # Custom RSpec runner for GlassSphere.
+    class Runner < ::RSpec::Core::Runner
+      def self.prepare
+        config['runner_class'].load_execution_map
+      end
+
+      def self.config
+        config_options = if config_file
+                           YAML.safe_load(config_file.read)
+                         else
+                           {}
+                         end
+
+        Configuration.new(config_options)
+      end
+
+      def self.config_file # rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength
+        env_file = ENV['GLASS_SPHERE_CONFIG']&.then { Pathname.new(_1) }
+        root_file = Pathname.new('glass_sphere.yml')
+        config_file = Pathname.new('config/glass_sphere.yml')
+
+        case
+        when env_file&.exist? then env_file
+        when root_file.exist? then root_file
+        when config_file.exist? then config_file
+        end
       end
     end
   end
--- spec/lib/rspec/runner_spec.rb
+++ spec/lib/rspec/runner_spec.rb
@@ -1,4 +1,66 @@
 # frozen_string_literal: true
 
 RSpec.describe GlassSphere::RSpec::Runner do
+  describe '.config' do
+    specify do
+      expect(described_class.config).to be_a(GlassSphere::RSpec::Runner::Configuration)
+    end
+
+    context 'when config file does not exist' do
+      it 'returns a Configuration with default values', :aggregate_failures do
+        allow(GlassSphere::RSpec::Runner::Configuration).to receive(:new).and_call_original
+
+        expect(described_class.config).to be_a(GlassSphere::RSpec::Runner::Configuration)
+        expect(GlassSphere::RSpec::Runner::Configuration).to have_received(:new).with({})
+      end
+    end
+
+    context 'when config file exists' do
+      let(:file_double) { instance_double(Pathname, exist?: true, read: '{}') }
+
+      it 'returns a Configuration with default values', :aggregate_failures do
+        allow(Pathname).to receive(:new).and_call_original
+        allow(Pathname).to receive(:new).with('glass_sphere.yml').and_return(file_double)
+        allow(GlassSphere::RSpec::Runner::Configuration).to receive(:new).and_call_original
+
+        expect(described_class.config).to be_a(GlassSphere::RSpec::Runner::Configuration)
+        expect(GlassSphere::RSpec::Runner::Configuration).to have_received(:new).with({})
+      end
+    end
+  end
+
+  describe '.config_file' do
+    let(:file_double) { instance_double(Pathname, exist?: true) }
+    let(:env_file) { 'spec/fixtures/glass_sphere.yml' }
+    let(:root_file) { 'glass_sphere.yml' }
+    let(:config_file) { 'config/glass_sphere.yml' }
+
+    context 'when GLASS_SPHERE_CONFIG is set and file exists' do
+      it 'returns a Pathname for said file' do
+        allow(ENV).to receive(:[]).with('GLASS_SPHERE_CONFIG').and_return(env_file)
+        allow(Pathname).to receive(:new).and_call_original
+        allow(Pathname).to receive(:new).with(env_file).and_return(file_double)
+
+        expect(described_class.config_file).to eq(file_double)
+      end
+    end
+
+    context 'when root file exists' do
+      it 'returns a Pathname for said file' do
+        allow(Pathname).to receive(:new).and_call_original
+        allow(Pathname).to receive(:new).with(root_file).and_return(file_double)
+
+        expect(described_class.config_file).to eq(file_double)
+      end
+    end
+
+    context 'when root file does not exist and config file exists' do
+      it 'returns a Pathname for said file' do
+        allow(Pathname).to receive(:new).and_call_original
+        allow(Pathname).to receive(:new).with(config_file).and_return(file_double)
+
+        expect(described_class.config_file).to eq(file_double)
+      end
+    end
+  end
 end
