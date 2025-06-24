e18d0e9739c819e3cf4f6a459e12750d0191ebae
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-24 14:29:30 -0400

    Add `GlassSphere::RSpec::Runner`

---
+++ lib/glass_sphere/rspec/runner.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module GlassSphere
+  module RSpec
+    module Runner
+      def self.invoke
+        todo!('GlassSphere::RSpec::Runner.invoke')
+      end
+    end
+  end
+end
